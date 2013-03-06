1dd067e9ad4a2efba0fde07ad7dd07b9fdb2bbee
Jakob Henriksson <jhenriksson@i-a-i.com>
2013-03-06 16:30:24 -0500

    removed unused classes and added menu with 'manage service' item

---
+++ plugins/com.iai.proteus/src/com/iai/proteus/command/ManageServicesHandler.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2013 Intelligent Automation Inc. 
+ * 
+ * All Rights Reserved.
+ */
+package com.iai.proteus.command;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+import com.iai.proteus.dialogs.ManageAllServicesDialog;
+import com.iai.proteus.ui.UIUtil;
+
+/**
+ * Handler for managing services 
+ * 
+ * @author Jakob Henriksson
+ *
+ */
+public class ManageServicesHandler implements IHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		// create and open the dialog to manage services 
+		ManageAllServicesDialog dialog =
+				new ManageAllServicesDialog(UIUtil.getShell());
+		dialog.open();
+		
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+
+	}
+
+}
